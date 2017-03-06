8e03806d104a93811d950c32f49023463376239b
root <254274509@qq.com>
2017-03-06 09:07:46 +0800

    首次部署项目到远程仓库

---
+++ shared/config/development/domain.php
@@ -0,0 +1,36 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config = array(
+	'base' => array(
+		'name' => '前台网站域名',
+		'url' => 'http://app.phpxueyuan.cn'
+	),
+	
+	'admin' => array(
+		'name' => '后台网站域名',
+		'url' => 'http://admin.phpxueyuan.cn'
+	),
+	
+	'service' => array(
+		'name' => '后台服务域名',
+		'url' => 'http://service.phpxueyuan.cn'
+	),
+		
+	'static' => array(
+		'name' => '静态资源域名',
+		'url' => 'http://static.phpxueyuan.cn'
+	),
+	
+	'mobile' => array(
+			'name' => '手机wap站',
+			'url' => 'http://m.phpxueyuan.cn'
+	),
+	'img' => array(
+			'name' => '图片访问域名',
+			'url' => 'http://img.phpxueyuan.cn',
+	),
+	'upload' => array(
+			'name' => '上传图片域名',
+			'url' => 'http://upload.phpxueyuan.cn',
+	),
+	
+);
